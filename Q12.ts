976c3a8d67d39f1d539bc0e64cefa9cfdf0b8eb2
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:47:47 -0700

    Add files via upload
    Q 12 Node.js of 45 Assignments

---
+++ Q12.ts
@@ -0,0 +1,13 @@
+// 12. Greeting: Start with the array you used in Exersice 11, but instead of just printing each
+//  person's name, Print a message to them. Thee text of each message should be the same, 
+//  but each should be personalalized with the person's name.
+
+let names: string[] = ["Sadiq", "Salman", "Farooq", "Umar"]
+
+let message: string = "How was the day Dear?";
+
+console.log(names[0] + " " + message)
+console.log(names[1] + " " + message)
+console.log(names[2] + " " + message)
+console.log(names[3] + " " + message)
+
